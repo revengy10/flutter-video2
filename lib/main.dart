0691d688204d9ca6616ddd042ecdae08a5ff3176
Emirhan Cosgun <emirhancsgn10@gmail.com>
2026-05-06 17:21:48 +0300

    Video 5-6: ListView Part 1 & Part 2

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,25 @@ void main() {
   runApp(MyApp());
 }
 
+
+class Post {
+  String body;
+  String author;
+  int likes = 0;
+  bool userLiked = false;
+  
+  Post({required this.body, required this.author});
+
+  void likePost() {
+    this.userLiked = !this.userLiked;
+    if(this.userLiked) {
+      this.likes += 1;
+    } else{
+      this.likes -= 1;
+    }
+  }
+}
+
 class MyApp extends StatelessWidget {
 
   @override
@@ -26,11 +45,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-String text = "";
+  List<Post> posts = [];
 
-void changeText(String text) {
+void newPost(String text) {
   this.setState(() {
-  this.text = text;
+    posts.add(Post(body: text, author:  "Emir"));
   });
 }
 
@@ -39,9 +58,10 @@ void changeText(String text) {
   Widget build(BuildContext context){
     return Scaffold(
     appBar: AppBar(title: Text('Hello World!')),
-    body: Column(children: <Widget>
-    [TextInputWiget(this.changeText), 
-    Text(this.text)]));
+    body: Column(children: <Widget>[
+    Expanded(child : PostList(this.posts)),
+    TextInputWiget(this.newPost)
+    ]));
   }
 }
 
@@ -65,8 +85,9 @@ class _TextInputWigetState extends State<TextInputWiget> {
   }
 
 void click() {
-  widget.callback(controller.text);
+  //FocusScope.of(context).unfocus();
   controller.clear();
+  widget.callback(controller.text);
 }
 
   @override
@@ -82,5 +103,47 @@ void click() {
         tooltip: "Post message",
       onPressed: this.click,
       )));
+  }
+}
+
+class PostList extends StatefulWidget {
+  final List<Post> listItems;
+
+  PostList(this.listItems);
+
+  @override
+  State<PostList> createState() => _PostListState();
+}
+
+class _PostListState extends State<PostList> {
+  void like(Function callBack) {
+    this.setState(() {
+      callBack();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount : this.widget.listItems.length,
+      itemBuilder: (context, index){
+        var post = this.widget.listItems[index];
+        return Card(child: Row(children: <Widget>[Expanded(child: ListTile(title: Text(post.body),
+        subtitle: Text(post.author),
+        )), 
+        Row(children: <Widget>[
+          Container(child: Text(post.likes.toString(),
+           style: TextStyle(fontSize: 20)),
+           padding: EdgeInsets.fromLTRB(0, 0, 10, 0),),
+        IconButton(
+          icon: Icon(Icons.thumb_up),
+          onPressed: () => this.like(post.likePost),
+          color: post.userLiked ? Colors.green : Colors.black
+        )
+        ],
+        )
+        ]));
+      },
+    );
   }
 }
